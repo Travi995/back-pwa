7a9fc8c2e5b8493d40d89c81d21c8f85232f0ebd
Travi995 <joseantoniotravieso259@gmail.com>
2024-12-07 17:32:49 -0500

    feat(auth): generate jwt

--- src/controllers/auth.controler.ts
+++ src/controllers/auth.controler.ts
@@ -1,51 +1,54 @@
 import { Request, Response } from "express";
 import { User } from "../entities/user";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../helpers/generateToken";
 
-export const postregister = async(req:Request, res:Response) => {
-    
+export const postregister = async (req: Request, res: Response) => {
+
     try {
-        const {name,email,password} = req.body
+        const { name, email, password } = req.body
 
-        const salt  =  bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(password,salt)
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
 
         const newUser = User.create({
             name,
             email,
-            password:hash
+            password: hash
         });
         await newUser.save();
 
-        res.status(201).json({mensaje:"usuario Agregado con Exito"})
+        res.status(201).json({ mensaje: "usuario Agregado con Exito" })
 
     } catch (error) {
-        res.status(500).json({mensaje:"error"})
+        res.status(500).json({ mensaje: "error" })
     }
 }
 
-export const postLogin = async(req:Request, res:Response) => {
+export const postLogin = async (req: Request, res: Response) => {
 
     try {
-        const {email,password} = req.body
-        const user = await User.findOneBy({email})
-        if(!user){
-            res.status(401).json({mensaje:"email o contraseña incorrecta"})
+        const { email, password } = req.body
+        const user = await User.findOneBy({ email })
+        if (!user) {
+            res.status(401).json({ mensaje: "email o contraseña incorrecta" })
             return
-        }else{
-            //comprobando la contrasena
-            const isPasswordCorrect =  bcrypt.compareSync(password,user.password)
-            if(!isPasswordCorrect){
-                res.status(401).json({mensaje:" email o contraseña incorrecta"})
-                return
-            }else{
-                res.status(200).json({mensaje:"Usuario Logueado Correctamente",id:user.id})
-            }
         }
 
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+
+        if (!isPasswordCorrect) {
+            res.status(401).json({ mensaje: " email o contraseña incorrecta" })
+            return
+        } 
+
+        const token =  await generateToken(user.id)
+        res.status(200).json({ mensaje: "Usuario Logueado con exito", token })
+
+
     } catch (error) {
-        res.status(500).json({mensaje:"error" })
-        
+        res.status(500).json({ mensaje: "error" })
+
     }
 
 
